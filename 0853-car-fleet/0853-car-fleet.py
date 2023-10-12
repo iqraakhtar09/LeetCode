193ad16246bac30e656c7cd7027d58ee3a18c6de
Iqra Akhtar <80781512+iqraakhtar09@users.noreply.github.com>
2023-10-12 10:31:38 -0700

    Time: 766 ms (55.29%), Space: 38.4 MB (58.07%) - LeetHub

---
+++ 0853-car-fleet/0853-car-fleet.py
@@ -0,0 +1,12 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+
+        pair = [(p, s) for p, s in zip(position, speed)] #list comprehension
+        pair.sort(reverse=True)
+        stack = []
+        for p, s in pair:  # Reverse Sorted Order
+            stack.append((target - p) / s) #calculating time for each car
+            if len(stack) >= 2 and stack[-1] <= stack[-2]:
+                stack.pop()
+        return len(stack)
+#(target - postion)/speed
