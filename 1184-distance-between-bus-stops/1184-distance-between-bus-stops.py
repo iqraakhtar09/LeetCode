0978eebcb4cd3297e54825fe42d17ec8fdf895ee
Iqra Akhtar <80781512+iqraakhtar09@users.noreply.github.com>
2023-10-27 06:53:51 -0700

    Time: 57 ms (19.37%), Space: 17.4 MB (41.40%) - LeetHub

---
+++ 1184-distance-between-bus-stops/1184-distance-between-bus-stops.py
@@ -0,0 +1,14 @@
+class Solution:
+    def distanceBetweenBusStops(self, distance: List[int], start: int, destination: int) -> int:
+        clockwise_distance = 0
+        n = len(distance)
+        i = start
+        
+        while i != destination:
+            clockwise_distance += distance[i]
+            start += 1
+            i = start % n
+        
+        anticlockwise_distance = sum(distance) - clockwise_distance
+        
+        return min(clockwise_distance, anticlockwise_distance)
