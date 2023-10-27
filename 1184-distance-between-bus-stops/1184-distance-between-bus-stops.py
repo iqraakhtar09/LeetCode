59017daa857aa4372bb7ccb38ccadd79ef914c39
Iqra Akhtar <80781512+iqraakhtar09@users.noreply.github.com>
2023-10-27 11:25:50 -0700

    Time: 55 ms (30.27%), Space: 17.3 MB (81.84%) - LeetHub

--- 1184-distance-between-bus-stops/1184-distance-between-bus-stops.py
+++ 1184-distance-between-bus-stops/1184-distance-between-bus-stops.py
@@ -12,3 +12,18 @@ class Solution:
         anticlockwise_distance = sum(distance) - clockwise_distance
         
         return min(clockwise_distance, anticlockwise_distance)
+    
+#     clockwise_dist = 0
+        
+#         if start < destination:
+#             for i in range(start, destination):
+#                 clockwise_dist += distance[i]
+#         elif destination < start:
+#             for i in range(start-1, destination-1, -1):
+#                 clockwise_dist += distance[i]
+                
+#         total_dist = sum(distance)
+        
+#         anti_clock_dist = total_dist - clockwise_dist
+        
+#         return min(anti_clock_dist, clockwise_dist)
