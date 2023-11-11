cf353696f0a86a54f5cc5fb79607cab0f1c95f07
Iqra Akhtar <80781512+iqraakhtar09@users.noreply.github.com>
2023-11-11 07:11:41 -0800

    Time: 38 ms (55.96%), Space: 16.1 MB (61.90%) - LeetHub

--- 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.py
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.py
@@ -11,6 +11,8 @@ class Solution:
         if num == 1:
             counter += 1
         return counter
+    
+    #One of the ways to solve this problem is by checking how many times we have to divide the given number by 2. If we observe the example, we can see that whenever the division results in an odd number, the counter is incremented by 2, while for an even number the counter is incremented by 1. How can we check if the number is odd or even? Each time we divide the number by 2, we need to check the remainder. If we have a remainder, the number is odd else the number is even.
         
             
         
